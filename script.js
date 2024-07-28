35ba0ccb945ac4e6f3f31ed74fd153bf38736743
VaishnaviMisal123 <vaishnavi.misal2402@gmail.com>
2024-07-28 17:08:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+};
+
+document.querySelectorAll('.about .video-container .controls .control-btn').forEach(btn => {
+    btn.onclick = () =>{
+        let src = btn.getAttribute('data-src');
+        document.querySelectorAll('.about .video-container .video').src = src;
+    }
+});
+
+
+
